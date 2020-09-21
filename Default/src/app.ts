9d4a54d6684f46527bfb3e1454a4190e91e191f2
pedrocvm <pedrocvm2@gmail.com>
2020-09-21 14:08:39 -0300

    excluding unnecessary files

--- Default/src/app.ts
+++
@@ -1,7 +0,0 @@
-﻿import "bootstrap/dist/css/bootstrap.css";
-import "font-awesome/css/font-awesome.css";
-
-import { platformBrowserDynamic } from "angular-ts-decorators";
-import { AppModule } from "./app.module";
-
-platformBrowserDynamic().bootstrapModule(AppModule, { strictDi: true });
